718ba2e937a0868d202b125b09c852958605f76e
Apoorv Anand <apoorv7491@gmail.com>
2021-05-31 19:39:58 +0530

    Add utils - Create auth token for SMTP

---
+++ scripts/utils.R
@@ -0,0 +1,5 @@
+
+# Gmail Auth --------------------------------------------------------------
+
+create_smtp_creds_key(id = "gmail_creds", user = "apoorv@civicdatalab.in",provider = "gmail", overwrite = TRUE)
+
